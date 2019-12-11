31f15aff8c1aeeab73272d5e1b61b1b5197a0a9a
Frank <zzzhangyanjia@gmail.com>
2019-12-11 23:15:11 +0800

    Finish Niconico and MAL part

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 const _queryInput = document.getElementById("queryString");
 const _queryIFrame = document.getElementById("queryIFrame");
 const _queryIFrameDiv = document.getElementById("queryIFrameDiv");
+const _queryTableDiv = document.getElementById("queryTableDiv");
+const _queryTableError = document.getElementById("queryTableError");
+const _nicoSeeMore = document.getElementById("nicoLink");
+const _malSeeMore = document.getElementById("malLink");
 const _proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 
 let querySiteName = "";
 let queryString = "";
 let querySite = "";
 let queryURL = "";
-// let animeNotFound = false;
-let animeData = "";
-// let placeholderValue = generatePlaceholder();
-let placeholderValue = "default";
 
-_queryInput.placeholder = placeholderValue;
+let placeholderValue = "default";
+generatePlaceholder().then();
 
 function formSubmitted() {
+    clearResults();
     querySiteName = document.querySelector('input[name="querySite"]:checked');
     queryString = _queryInput.value;
     if (queryString === "") {
@@ -27,26 +29,64 @@ function formSubmitted() {
         _queryIFrameDiv.style.display = "initial";
     }
     else if (querySite === "niconico") {
-        queryURL = "https://api.search.nicovideo.jp/api/v2/snapshot/video/contents/search?q="+encodeURIComponent(queryString)+"&targets=title&_sort=-startTime&fields=contentId,title,thumbnailUrl,startTime&_limit=10";
-        getData(queryURL).then();
-        console.log(animeData["data"][0]);
-        let metaStatus = animeData["meta"]["status"];
-        if (metaStatus === 200) {
-            animeData = getData(queryURL);
-            for (let i = 1; i <= 10; i ++) {
-//                let j = i - 1;
-                let thumbID = "queryTableThumb" + i;
-                let titleID = "queryTableTitle" + i;
-                let timeID = "queryTableTime" + i;
-                console.log("ThumbID: "+thumbID);
-                console.log("TitleID: "+titleID);
-                console.log("timeID: "+timeID);
-//                document.getElementById(thumbID).setAttribute("src", animeData.data.[j].thumbnailUrl);
-            }
-        }
+        queryURL = _proxyUrl + "https://api.search.nicovideo.jp/api/v2/snapshot/video/contents/search?q=" + encodeURIComponent(queryString) + "&targets=title&_sort=-viewCounter&fields=contentId,title,thumbnailUrl,startTime&_limit=10&filters[startTime][gte]=2019-01-01T00:00:00";
+        return fetch(queryURL)
+            .then(res => res.json())
+            .then(info => {
+                if (info["meta"]["status"] === 200) {
+                    if (info["meta"]["totalCount"] === 0) {
+                        _queryTableError.style.display = "initial";
+                    }
+                    else {
+                        for (let i = 1; i <= 10; i++) {
+                            let j = i - 1;
+                            let id = info["data"][j]["contentId"];
+                            let title = info["data"][j]["title"];
+                            let thumb = info["data"][j]["thumbnailUrl"];
+                            let time = info["data"][j]["startTime"];
+                            let url = "https://nico.ms/"+id;
+                            let thumbE = "queryTableThumb"+i;
+                            let thumbLinkE = "queryTableThumbLink"+i;
+                            let titleE = "queryTableTitle"+i;
+                            let timeE = "queryTableTime"+i;
+                            document.getElementById(thumbE).src = thumb;
+                            document.getElementById(thumbE).alt = title;
+                            document.getElementById(titleE).innerHTML = title;
+                            document.getElementById(titleE).href = url;
+                            document.getElementById(thumbLinkE).href = url;
+                            document.getElementById(timeE).innerHTML = time;
+                        }
+                        _nicoSeeMore.style.display = "initial";
+                        _queryTableDiv.style.display = "initial";
+/*                        info.data.forEach(e => {
+                            console.log(e.contentId);
+                        })*/
+                    }
+                }
+            });
     }
     else if (querySite === "mal") {
-        // TODO: MAL API
+        queryURL = "https://api.jikan.moe/v3/search/anime?q=" + encodeURIComponent(queryString) + "&limit=10";
+        return fetch(queryURL)
+            .then(res => res.json())
+            .then(info => {
+                for (let i = 1; i <= 10; i++) {
+                    let j = i - 1;
+                    let url = info["results"][j]["url"];
+                    let title = info["results"][j]["title"];
+                    let thumb = info["results"][j]["image_url"];
+                    let thumbE = "queryTableThumb"+i;
+                    let thumbLinkE = "queryTableThumbLink"+i;
+                    let titleE = "queryTableTitle"+i;
+                    document.getElementById(thumbE).src = thumb;
+                    document.getElementById(thumbE).alt = title;
+                    document.getElementById(titleE).innerHTML = title;
+                    document.getElementById(titleE).href = url;
+                    document.getElementById(thumbLinkE).href = url;
+                }
+                _malSeeMore.style.display = "initial";
+                _queryTableDiv.style.display = "initial";
+            });
     }
     else if (querySite === "al") {
         // TODO: AL API
@@ -56,7 +96,53 @@ function formSubmitted() {
     }
 }
 
-async function getData(apiURL) {
+function nicoSeeMore() {
+    window.open("https://www.nicovideo.jp/search/" + queryString, "_blank");
+}
+
+function malSeeMore() {
+    window.open("https://myanimelist.net/search/all?q=" + queryString, "_blank");
+}
+
+function generatePlaceholder() {
+    // let id = Math.floor(Math.random() * 33993 + 1);
+    let id = Math.floor(Math.random() * Math.floor(16310));
+    queryURL = "https://api.jikan.moe/v3/anime/"+id;
+    return fetch(queryURL)
+        .then(res => res.json())
+        .then(info => {
+            let metaStatus = info.status;
+            if (metaStatus === 404) {
+                generatePlaceholder().then();
+            }
+            else if (metaStatus === 429) {
+                wait(2000);
+                generatePlaceholder().then();
+            }
+            else {
+                placeholderValue = info.title;
+                _queryInput.placeholder = placeholderValue;
+            }
+        });
+}
+
+function wait(ms){
+    const start = new Date().getTime();
+    let end = start;
+    while(end < start + ms) {
+        end = new Date().getTime();
+    }
+}
+
+function clearResults() {
+    _queryTableError.style.display = "none";
+    _queryIFrameDiv.style.display = "none";
+    _queryTableDiv.style.display = "none";
+    _nicoSeeMore.style.display = "none";
+    _malSeeMore.style.display = "none";
+}
+
+/*async function getData(apiURL) {
     apiURL = _proxyUrl + apiURL;
     fetch(apiURL)
         .then(response => response.json())
@@ -64,20 +150,6 @@ async function getData(apiURL) {
             console.log(json);
             animeData = json;
         });
-}
-
-function nicoSeeMore() {
-
-}
-
-/*async function generatePlaceholder() {
-    while (!animeNotFound) {
-        let id = Math.floor(Math.random() * 33993 + 1);
-        queryURL = "https://api.jikan.moe/v3/"+id;
-        animeData = getData(queryURL);
-        animeNotFound = animeData.status === "404";
-    }
-    return animeData.title;
 }*/
 
 // TODO: Load Kitsu API
